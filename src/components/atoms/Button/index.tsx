15c2a0dae248c6971acb9d329e0d4639f9be45c9
sudipstha08 <sudipstha088@gmail.com>
2022-11-16 22:42:09 +0545

    feat: add components

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button} from 'react-native'
+
+const ButtonComponent = () => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export  {ButtonComponent as Button }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Button } from 'react-native'
+
+const Home = () => {
+  return (
+    <View><Button title='Hello'/></View>
+  )
+}
+
+export  {Home}
